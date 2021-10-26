9b2d5940abf20b153b99c4d168ed7a879da0e696
yvonneonu <yvonneonuorah@gmail.com>
2021-10-27 00:05:57 +0100

    Login RegisterScreen

--- app/src/main/java/com/owo/phlurtyzpaid/activity/MakePayment.java
+++ app/src/main/java/com/owo/phlurtyzpaid/activity/MakePayment.java
@@ -1,10 +1,16 @@
 package com.owo.phlurtyzpaid.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.owo.phlurtyzpaid.R;
+
+import java.util.zip.Inflater;
 
 public class MakePayment extends AppCompatActivity {
 
@@ -12,5 +18,29 @@ public class MakePayment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_payment);
+
+
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+//         super.onCreateOptionsMenu(menu);
+
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.makepayment, menu);
+        getMenuInflater().inflate(R.menu.makepayment, menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
